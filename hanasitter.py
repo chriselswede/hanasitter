769ebd7d598d6739afb25961685c9a993202cf61
chriselswede <chriselswede@gmail.com>
2023-05-17 20:15:21 +0200

    Fixing wrong customer.sh
    Removing strange output in path

--- hanasitter.py
+++ hanasitter.py
@@ -1173,6 +1173,7 @@ def cdalias(alias, local_dbinstance, shell):   # alias e.g. cdtrace, cdhdb, ...
             piece = out.decode().strip("\n")
         path = path + '/' + piece + '/'
     path = path.replace("[0-9][0-9]", local_dbinstance) # if /bin/bash shows strange HDB[0-9][0-9] we force correct instance on it
+    path = path.replace("executing .customer.sh.....\n", '')  #removing strange stuff 
     return path    
         
 def log(message, comman, file_name = "", sendEmail = False):
